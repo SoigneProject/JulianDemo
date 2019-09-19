ef910800e785a2f3a2bb95dd175c9f606e1d60d9
Juju Liao <liaojulian@gmail.com>
2019-09-19 16:28:32 -0700

    Connected to DB and added user route NEED TO TEST

--- skeleton/root/backend/api/routes/userRoute.js
+++ skeleton/root/backend/api/routes/userRoute.js
@@ -6,8 +6,4 @@ var user_controller = require('../controllers/userController');
 router.get('/users', user_controller.get_all_users);
 router.post('/users', user_controller.create_a_user);
 
-// app.route('/users')
-//     .get(user.get_all_users)
-//     .post(user.create_a_user);
-
 module.exports = router;
--- skeleton/root/backend/server.js
+++ skeleton/root/backend/server.js
@@ -1,15 +1,37 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  userRoute = require('./api/routes/userRoute'),
+  bodyParser = require('body-parser');
+
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://Test:Test123@cs160-cluster-gigd4.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 client.connect(err => { //this will connect me to the database
 
-const db = client.db("Test");
- const collection = client.db("test").collection("devices");
- db.collection('Test').insertOne({
-  item: "canvas",
-  qty: 110,
-  tags: ["paper"],
-  size: { h: 28, w: 35.5, uom: "cm" }
-})
- client.close(); //this closes the client 
+  // const db = client.db("Test");
+  // const collection = client.db("test").collection("devices");
+  // db.collection('Test').insertOne({
+  //   item: "canvas",
+  //   qty: 110,
+  //   tags: ["paper"],
+  //   size: {
+  //     h: 28,
+  //     w: 35.5,
+  //     uom: "cm"
+  //   }
+  // })
+  client.close(); //this closes the client 
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/users', userRoute);
+
+app.listen(port);
+
+console.log('soigne server started on: ' + port);
